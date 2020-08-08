149e3e6d47cddcab1f362e96ac0f0196f22e20f6
matt merrill <mattmerrill@matts-MacBook-Pro.local>
2020-08-08 14:32:19 -0700

    refactored

--- ultimate_challenge/functions/graphing.py
+++ ultimate_challenge/functions/graphing.py
@@ -6,7 +6,7 @@ def get_pct_class_by_category(df, category, class_col):
     '''This function will output the percentage of classified values for the 
         categories in a given categorical variable'''
     
-    retained = df.groupby([category, 'is_adopted']).agg({'is_adopted': 'count'})
+    retained = df.groupby([category, class_col]).agg({class_col: 'count'})
     retained_pct = retained.groupby(level=0).apply(lambda x: 100*x/float(x.sum()))
     
     return retained_pct
@@ -47,22 +47,22 @@ def overlayed_labeled_bar_graph(df, category, class_col):
     plt.bar(x, y1)
     plt.bar(x, y2)
     
-    plt.title("{} adoption rates by category".format(category), size=16)
+    plt.title("{} retention rates by category".format(category), size=16)
     
     # specific for this project
-    plt.legend(['Not adopted', 'Adopted'])
+    plt.legend(['Not retained', 'Retained'])
     
     for i, title, label in zip(range(len(x)), x, bar_labels):
         ax.text(title, y1[i], label, ha="center", **style)
 
 
-def timeseries_frequency_plot(title, df, timeseries_col, y_col, dStart, dEnd, sample_by = 'D', xtick_freq = 'W'):
+def timeseries_frequency_plot(title, df, timeseries_col, y_col, dStart, dEnd, sample_by='D', xtick_freq ='W', axvline=[np.nan]):
     
     '''This function will aggregate a dataframe by count given two columns, the datetime column and any column used to 
     determine frequency'''
     
     timeseries_vs_y = df[[timeseries_col, y_col]]
-    plot_df = timeseries_vs_y.set_index(timeseries_col).resample(sample_by)[y_col].sum()
+    plot_df = timeseries_vs_y.set_index(timeseries_col).resample(sample_by)[y_col].count()
     df = plot_df[(dStart <= plot_df.index) & (plot_df.index <= dEnd)]
 
     ax = plt.figure(figsize=(20, 6)).add_subplot(111)   
@@ -72,6 +72,9 @@ def timeseries_frequency_plot(title, df, timeseries_col, y_col, dStart, dEnd, sa
     
     ax.set_title(title, fontsize=16)
     ax.set_xticklabels([x.strftime('%h%d\n%a\n%-I%p\n%Y') for x in xticks])
+    
+    for axv in axvline:
+        ax.axvline(axv, color='r', linestyle='--', lw=2)
     
     ax.tick_params(axis='x', which='major', pad=15)
     plt.setp(ax.xaxis.get_majorticklabels(), ha='right')
@@ -112,16 +115,16 @@ def hist_ecdf_pair_plot(df, col, class_col, title, bin_size = 20):
     
     plt.figure(figsize=(12, 4))
     plt.title(title, size=15)
-    plt.hist(df[col], bins=bin_size)
+    plt.hist(df[df[class_col] == 0][col], bins=bin_size)
     plt.hist(df[df[class_col] == 1][col], bins=bin_size)
     plt.ylabel('Frequency')
-    plt.legend(['retained', 'not retained'])
+    plt.legend(['not retained', 'retained'])
     
     plt.figure(figsize=(12, 4))
-    plt.plot(x_active, y_active, marker='.', linestyle='none')
     plt.plot(x_inactive, y_inactive, marker='.', linestyle='none')
+    plt.plot(x_active, y_active, marker='.', linestyle='none')
     plt.xlabel(col)
     plt.ylabel('ECDF')
-    plt.legend(['retained', 'not retained'])
+    plt.legend(['not retained', 'retained'])
     
     plt.show()
